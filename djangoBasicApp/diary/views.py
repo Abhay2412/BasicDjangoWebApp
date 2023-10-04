43a63ebbaac3b35c263275b0f68c3c063c3765d7
Abhay2412 <abhaykhosla0@gmail.com>
2023-10-03 21:44:27 -0600

    Work on integrating HTML/CSS with templates in Django

--- djangoBasicApp/diary/views.py
+++ djangoBasicApp/diary/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+diaryPosts = [
+    {
+        'author': 'John Cena',
+        'title': 'Diary Post 1',
+        'content': 'First Diary post content',
+        'date_posted': 'October 3, 2023'
+    },
+    {
+        'author': 'Randy Orton',
+        'title': 'Diary Post 2',
+        'content': 'Second Diary post content',
+        'date_posted': 'October 3, 2023'
+    }
+]
+
 
 
 def home(request):
-    return HttpResponse('<h1>Diary Home</h1>')
+    context = {
+        'diaryPosts': diaryPosts
+    }
+    return render(request, 'diary/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Diary About</h1>')
+    return render(request, 'diary/about.html', {'title': 'About'})

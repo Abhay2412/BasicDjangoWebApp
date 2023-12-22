33e155747f2bd24c168ed063652fca53e338e2b8
Abhay2412 <abhaykhosla0@gmail.com>
2023-12-21 18:28:02 -0700

    Learn how to create, update and delete using forms

--- djangoBasicApp/diary/urls.py
+++ djangoBasicApp/diary/urls.py
@@ -1,7 +1,26 @@
 from django.urls import path
+from .views import (
+    DiaryPostListView,
+    DiaryPostDetailView,
+    DiaryPostCreateView,
+    DiaryPostUpdateView,
+    DiaryPostDeleteView,
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='diary-home'),
-    path('about/', views.about, name='diary-about'),
+    path("", DiaryPostListView.as_view(), name="diary-home"),
+    path("diaryPost/<int:pk>/", DiaryPostDetailView.as_view(), name="diaryPost-detail"),
+    path("diaryPost/new/", DiaryPostCreateView.as_view(), name="diaryPost-create"),
+    path(
+        "diaryPost/<int:pk>/update/",
+        DiaryPostUpdateView.as_view(),
+        name="diaryPost-update",
+    ),
+    path(
+        "diaryPost/<int:pk>/delete/",
+        DiaryPostDeleteView.as_view(),
+        name="diaryPost-delete",
+    ),
+    path("about/", views.about, name="diary-about"),
 ]
--- djangoBasicApp/diary/views.py
+++ djangoBasicApp/diary/views.py
@@ -1,12 +1,65 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import DiaryPost
 
 
 def home(request):
-    context = {
-        'diaryPosts': DiaryPost.objects.all()
-    }
-    return render(request, 'diary/home.html', context)
+    context = {"diaryPosts": DiaryPost.objects.all()}
+    return render(request, "diary/home.html", context)
+
+
+class DiaryPostListView(ListView):
+    model = DiaryPost
+    template_name = "diary/home.html"
+    context_object_name = "diaryPosts"
+    ordering = ["-date_posted"]
+
+
+class DiaryPostDetailView(DetailView):
+    model = DiaryPost
+
+
+class DiaryPostCreateView(LoginRequiredMixin, CreateView):
+    model = DiaryPost
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class DiaryPostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = DiaryPost
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        diaryPost = self.get_object()
+        if self.request.user == diaryPost.author:
+            return True
+        return False
+
+
+class DiaryPostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = DiaryPost
+    success_url = "/"
+
+    def test_func(self):
+        diaryPost = self.get_object()
+        if self.request.user == diaryPost.author:
+            return True
+        return False
+
 
 def about(request):
-    return render(request, 'diary/about.html', {'title': 'About'})
+    return render(request, "diary/about.html", {"title": "About"})
